044e55756371b219827060d728720ca5d21a8c11
wignn <tigfiragnafatur1933@gmail.com>
2024-11-14 21:26:17 +0700

    init

--- Big_Black_Box/Big_Black_Box/C/FUncion_void.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void kali(int a,int b,int c){
-    c=a*b;
-    printf("%d")
-}
--- Big_Black_Box/Big_Black_Box/PY/main.py
+++
@@ -1,9 +0,0 @@
-
-print ("\nprogram menghitung volume balok\n")
-
-panjang = float (input('masukan panjang dalam cm: '))
-tinggi = float (input('masukan tinggi dalam cm: '))
-lebar = float (input('masukan lebar dalam cm: '))
-    
-volume = panjang * tinggi * lebar
-print("volume balok adalah", volume, "cm")
